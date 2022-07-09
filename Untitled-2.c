89bb23c1c7261c6ed5a61a3476d1244e5c0e97c5
simple <ayush.raj021@gmail.com>
2022-07-09 18:20:48 +0530

    program

---
+++ Untitled-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+   int a,b;
+   printf("TYPE NUMBER");
+   scanf("%d",&a);
+   printf("TYPE 2ND NUMBER");
+   scanf("%d",&b);
+
+   int c=a+b;
+   printf("SUM OF TWO NUMBERS %d ",c);
+   return 0;
+}
