492ad0e67f7ddf770aab5c7bc9f91be94fb74d88
simple <ayush.raj021@gmail.com>
2022-07-09 18:26:20 +0530

    2xprogram

--- Untitled-2.c
+++ Untitled-2.c
@@ -11,3 +11,16 @@ int main()
    printf("SUM OF TWO NUMBERS %d ",c);
    return 0;
 }
+#include<stdio.h>
+int main()
+{
+   int a,b;
+   printf("TYPE NUMBER");
+   scanf("%d",&a);
+   printf("TYPE 2ND NUMBER");
+   scanf("%d",&b);
+
+   int c=a+b;
+   printf("SUM OF TWO NUMBERS %d ",c);
+   return 0;
+}
